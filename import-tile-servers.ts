fc802950019b3ce3851c85a478fa855c7f78e963
Simon Levente-Attila <simonleventeattila@gmail.com>
2020-10-15 02:22:54 +0300

    Trying to use lodash to get rid of new line char

--- import-tile-servers.ts
+++ import-tile-servers.ts
@@ -1,11 +1,30 @@
 require ('isomorphic-fetch');
+const _ = require("lodash-es");
 const fs = require("fs");
 let data:object = {};
-let gsheets = require('gsheets');
-let gsheetsUrl:string ='1SE6wf-cD2vvWMUq7gYx-QFD7ZUit-wSh4DaRq3vY61A'; //Id of the TileServers google spreadsheet (https://docs.google.com/spreadsheets/d/1SE6wf-cD2vvWMUq7gYx-QFD7ZUit-wSh4DaRq3vY61A/edit?ts=5f7f66a3#gid=0)
-let workSheet:string = "tileServers"                                   
+import * as gsheets from "gsheets";
+const gsheetsUrl:string ='1SE6wf-cD2vvWMUq7gYx-QFD7ZUit-wSh4DaRq3vY61A'; //Id of the TileServers google spreadsheet (https://docs.google.com/spreadsheets/d/1SE6wf-cD2vvWMUq7gYx-QFD7ZUit-wSh4DaRq3vY61A/edit?ts=5f7f66a3#gid=0)
+const workSheet:string = "tileServers"  ;                                 
+interface TileData {
+    data: Array<any>;
+    
+}
 
-data = gsheets.getWorksheet(gsheetsUrl, workSheet).then(function (res) {                            
-    let data = JSON.stringify(res, null, 4);
-    fs.writeFileSync("./src/assets/tile-sources.json", data);
-        return console.debug("Import complete"); }, function (err) { return console.error(err); });
+
+
+data = gsheets.getWorksheet(gsheetsUrl, workSheet).then((response: TileData) => {                            
+    //const data: TileData = response as TileData;
+    response.data = response.data.filter(elem  => elem.enabled === "yes" || elem.enabled === "TRUE");
+    let a : string = "";
+    a.replace[Symbol]_
+    response.data.forEach(elem => {
+        delete elem.latency, delete elem.provider;
+        delete elem.enabled, delete elem.description; //Yes, for sure there is a better way. Calling 'delete' on an array which is made up of all the element we want to get rid of.
+        delete elem.IPv6, delete elem.tileUrlSample;
+        delete elem.sourceCode, delete elem.language;
+        
+        
+    });
+    fs.writeFileSync("./src/assets/tile-sources.json", JSON.stringify(response, null, 4));
+    return console.debug("Import complete"); }, function (err) { return console.error(err);
+});
